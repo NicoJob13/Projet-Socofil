300baca1485191dca12e2cba709f03e968baabca
NicoJob13 <74857678+NicoJob13@users.noreply.github.com>
2022-02-24 13:39:15 +0100

    Sécurisation

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,39 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const path = require("path");
+
 const { checkAuth, requireAuth } = require("./middleware/auth.middlewares");
 
 require("./config/db");
-
-const app = express();
+require("dotenv").config({ path: "./config/.env" });
 
 const userRoutes = require("./routes/user.routes");
 const postRoutes = require("./routes/post.routes");
+
+const app = express();
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  allowedHeaders: ["sessionId", "Content-Type"],
+  exposedHeaders: ["sessionId"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+};
+app.use(cors(corsOptions));
+
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMS: 5 * 60 * 1000,
+  max: 50,
+  message: "Too many requests made with this IP address",
+});
+
+app.use(limiter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
