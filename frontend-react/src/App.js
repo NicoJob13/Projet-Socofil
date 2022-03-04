dadc8609061cb617bf724f6dc8a3808fba9a3f25
NicoJob13 <74857678+NicoJob13@users.noreply.github.com>
2022-03-04 13:43:14 +0100

    Routage

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Routes from "./components/Routes";
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+};
 
 export default App;
